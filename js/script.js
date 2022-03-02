00c8626e1db47aa8a7bcf18adfb8df219116ebf1
Njoki <kinuthiash@gmail.com>
2022-03-02 15:38:53 +0300

    feat:js-add sum function

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var sum = (num1,num2)=>{
+  return num1+num2;
+}
+var age1 = parseInt(prompt("How old is John? "));
+var age2 = parseInt(prompt("How old is Mary? "));
+alert("the sum of their ages is " + sum(age1,age2));
