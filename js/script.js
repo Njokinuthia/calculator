e5888d7e8003e2059a8d3c81c0ff3f0553516847
Njoki <kinuthiash@gmail.com>
2022-03-02 16:03:45 +0300

    feat:js-add functions for other functions

--- js/script.js
+++ js/script.js
@@ -1,10 +1,25 @@
+// prompt user for numbers/input
+var age1 = parseInt(prompt("How old is John? "));
+var age2 = parseInt(prompt("How old is Mary? "));
+
 // sum funtion
 var sum = (num1,num2)=>{
   return num1+num2;
 }
-var age1 = parseInt(prompt("How old is John? "));
-var age2 = parseInt(prompt("How old is Mary? "));
-var result = sum(age1,age2);
+// subtract function
+var subtract = (num1,num2)=>{
+  return num1-num2;
+}
+// multiplication function
+var multiply = (num1,num2)=>{
+  return num1*num2;
+}
+// division function
+var divide = (num1,num2) =>{
+  return num1/num2;
+}
+
+// output the result
+var result = divide(age1,age2);
 alert(result);
 
-// 
