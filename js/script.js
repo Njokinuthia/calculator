1a22942f370fd20d6d66fcef623df4fde5af7eed
Njoki <kinuthiash@gmail.com>
2022-03-17 21:23:02 +0300

    feat:add UI sum function

--- js/script.js
+++ js/script.js
@@ -6,6 +6,16 @@ const multiply = (num1,num2)=>num1*num2;
 const divide = (num1,num2) =>num1/num2;
 
 // FRONT END LOGIC - USER INTERFACE
+// submit form
+$(document).ready(function(){
+  $("form#addNum").submit(function(event){
+    event.preventDefault();    
+    let add1 =parseInt(document.getElementById("add1").value);
+    let add2 = parseInt(document.getElementById("add2").value);
+    alert(sum(add1,add2));    
+  })
+})
+
 
 // output the result
 var result = sum(age1,age2);
