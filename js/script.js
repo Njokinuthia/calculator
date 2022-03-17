695563dcb8463e7af27fb7fa1f01b32251a63013
Njoki <kinuthiash@gmail.com>
2022-03-17 21:14:48 +0300

    feat:form-add forms

--- js/script.js
+++ js/script.js
@@ -1,26 +1,11 @@
 // BACKEND LOGIC - BUSINESS LOGIC
-// sum funtion
-var sum = (num1,num2)=>{
-  return num1+num2;
-}
-// subtract function
-var subtract = (num1,num2)=>{
-  return num1-num2;
-}
-// multiplication function
-var multiply = (num1,num2)=>{
-  return num1*num2;
-}
-// division function
-var divide = (num1,num2) =>{
-  return num1/num2;
-}
+// calculation funtions
+const sum = (num1,num2)=>num1+num2;
+const subtract = (num1,num2)=>num1-num2;
+const multiply = (num1,num2)=>num1*num2;
+const divide = (num1,num2) =>num1/num2;
 
 // FRONT END LOGIC - USER INTERFACE
-
-// prompt user for numbers/input
-// var age1 = parseInt(prompt("How old is John? "));
-// var age2 = parseInt(prompt("How old is Mary? "));
 
 // output the result
 var result = sum(age1,age2);
